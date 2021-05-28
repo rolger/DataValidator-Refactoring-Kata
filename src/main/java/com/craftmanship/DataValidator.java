25c6b672ab8857a50caebd5136de72fb2b0d7586
rolger <roland.germ@gmail.com>
2021-05-28 16:21:59 +0200

    first try bottom up approach

--- src/main/java/com/craftmanship/DataValidator.java
+++ src/main/java/com/craftmanship/DataValidator.java
@@ -6,6 +6,46 @@ import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Predicate;
+
+// Vergleichbare andere Katas:
+// Email Sender code refactroing kata
+// error conditions refactroing kata?
+// ValidateAndAddProduct-Refactoring-Kata
+
+// Mögliche Änderungen:
+// - neue Columns
+// - Änderungen in der Reihenfolge der Columns
+// - Änderungen/Fehler in Validierung
+// - Mehrere Validierungen für ein Feld
+
+// welches Design Möglichkeiten stehen zur Verfügung 
+//  Liste von commands
+//  Liste von Specifications
+//  Monade
+//  Composite Pattern
+//  Template Method
+//  Vavr Validation Framework?
+
+//v1
+//List<ErrorInfo> errors = new ValidatorCollector()
+//	.validate(t -> Validators::validFirstName, "first name must contain characters", columns.get(0))
+//	.validate(t -> Validators::validLastName, "last name must contain characters", columns.get(1))
+//	.validate(t -> Validators::validXYZ, "last name must contain characters", columns.get(4))
+//	.errors();
+	
+//v2	
+//List<ValidationSpec> specs = List.of(
+//	new ValidationSpec(t -> Validators::validFirstName, "first name must contain characters", columns.get(0))
+//	new ValidationSpec(t -> Validators::validXYZ, "last name must contain characters", columns.get(4))
+//);
+//List<ErrorInfo> errors = specs.stream.flatMap(ValidationSpec::valdiate).collect(....);
+
+
+// welche Wege stehen uns zur Verfügung
+// bottom up refactoring
+// top down
+// mixed?
 
 public class DataValidator {
 
@@ -21,24 +61,49 @@ public class DataValidator {
 		data.forEach((row, columns) -> {
 
 			if (columns != null && !columns.isEmpty()) {
-				if (!validName(columns.get(0))) {
-					errors.add(new ErrorInfo(row, "first name must contain characters"));
+				{
+					String value = columns.get(0);
+					String message = "first name must contain characters";
+					Predicate<String> validator = t -> validFirstName(t);
+
+					List<ErrorInfo> errorInfo = validateField(row, value, message, validator);
+					errors.addAll(errorInfo);
 				}
 
-				if (!validName(columns.get(1))) {
-					errors.add(new ErrorInfo(row, "last name must contain characters"));
+				{
+					String value = columns.get(1);
+					String message = "last name must contain characters";
+					Predicate<String> validator = t -> validLastName(t);
+
+					List<ErrorInfo> errorInfo = validateField(row, value, message, validator);
+					errors.addAll(errorInfo);
 				}
 
-				if (!validC(columns.get(2))) {
-					errors.add(new ErrorInfo(row, String.format("country code (%s) doesn't exist", columns.get(2))));
+				{
+					String message = String.format("country code (%s) doesn't exist", columns.get(2));
+					String value = columns.get(2);
+					Predicate<String> validator = t -> validC(t);
+
+					List<ErrorInfo> errorInfo = validateField(row, value, message, validator);
+					errors.addAll(errorInfo);
 				}
 
-				if (!validDate(columns.get(3))) {
-					errors.add(new ErrorInfo(row, String.format("birthdate (%s) can not be parsed", columns.get(3))));
+				{
+					String value = columns.get(3);
+					String message = String.format("birthdate (%s) can not be parsed", columns.get(3));
+					Predicate<String> validator = t -> validDate(t);
+
+					List<ErrorInfo> errorInfo = validateField(row, value, message, validator);
+					errors.addAll(errorInfo);
 				}
 
-				if (invalidMoney(columns.get(4))) {
-					errors.add(new ErrorInfo(row, String.format("income (%s) can not be parsed", columns.get(4))));
+				{
+					String value = columns.get(4);
+					String message = String.format("income (%s) can not be parsed", columns.get(4));
+					Predicate<String> validator = t -> validMoney(t);
+
+					List<ErrorInfo> errorInfo = validateField(row, value, message, validator);
+					errors.addAll(errorInfo);
 				}
 			}
 
@@ -46,8 +111,24 @@ public class DataValidator {
 		return errors;
 	}
 
+	private boolean validFirstName(String t) {
+		return validName(t);
+	}
+
+	private boolean validLastName(String t) {
+		return validName(t);
+	}
+
 	private boolean validName(String value) {
 		return value != null && !value.isBlank() && value.chars().allMatch(Character::isAlphabetic);
+	}
+	
+	private List<ErrorInfo> validateField(Integer row, String value, String message, Predicate<String> validator) {
+		List<ErrorInfo> errorInfo = new ArrayList<>();
+		if (!validator.test(value)) {
+			errorInfo.add(new ErrorInfo(row, message));
+		}
+		return errorInfo;
 	}
 
 	private boolean validC(String string) {
@@ -57,21 +138,21 @@ public class DataValidator {
 	private static boolean validDate(String string) {
 		try {
 			LocalDate.parse(string);
-		} catch (DateTimeParseException  e) {
+		} catch (DateTimeParseException e) {
 			return false;
 		}
 		return true;
 	}
 
-	private boolean invalidMoney(String s) {
+	private boolean validMoney(String s) {
 		try {
 			BigDecimal money = new BigDecimal(s);
 			if (money.compareTo(BigDecimal.ZERO) == -1) {
-				return true;
+				return false;
 			}
-			return false;
-		} catch (NumberFormatException e) {
 			return true;
+		} catch (NumberFormatException e) {
+			return false;
 		}
 	}
 
