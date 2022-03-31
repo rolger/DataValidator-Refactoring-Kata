30f1343b4eb15144dcd146865fee68704e69d22c
rgerm <roland.germ@admiral.at>
2022-03-31 10:12:45 +0200

    refactor: clean up test files

--- java/src/test/java/com/craftmanship/DataValidatorTest.java
+++ java/src/test/java/com/craftmanship/DataValidatorTest.java
@@ -1,64 +1,63 @@
 package com.craftmanship;
 
-import static org.approvaltests.Approvals.verify;
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import org.junit.jupiter.api.Test;
+import static java.util.Map.of;
+import static org.approvaltests.Approvals.verify;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class DataValidatorTest {
 
-	@Test
-	void shouldContainNoErrorsIfDataIsValid() {
-		Map<Integer, List<String>> data = Map.of(1, List.of("Susi", "Sunshine", "AT", "1971-12-04", "2300.20"));
-		
-		List<ErrorInfo> errors = new DataValidator(withCountry("AT")).check(data);
-		
-		assertThat(errors).isEmpty();
-	}
+    @Test
+    void shouldContainNoErrorsIfDataIsValid() {
+        Map<Integer, List<String>> data = of(1, List.of("Susi", "Sunshine", "AT", "1971-12-04", "2300.20"));
 
-	
-	@Test
-	void shouldContainNoErrorsIfDataIsEmpty() {
-		Map<Integer, List<String>> data = new HashMap<>();
-		
-		data.put(0, null);
-		data.put(1, List.of());
-		
-		List<ErrorInfo> errors = new DataValidator(withCountry("AT")).check(data);
-		
-		assertThat(errors).isEmpty();
-	}
-	
-	@Test
-	void shouldContainErrorsForEachInvalidEntry() {
-		Map<Integer, List<String>> data = Map.of(
-				1, List.of("Susi", "", "AT", "1971-12-04", "2300.20"), 
-				2, List.of("", "Sunshine", "AT", "1971-12-04", "2300.20"),
-				3, List.of("A1", "Sunshine", "AT", "1971-12-04", "2300.20"),
-				4, List.of("Susi", "Sunshine", "DE", "1971-12-04", "2300.20"),
-				5, List.of("Susi", "Sunshine", "AT", "not a date", "2300.20"),
-				6, List.of("Susi", "Sunshine", "AT", "1971-12-04", "not a BigDecimal"),
-				7, List.of("Susi", "Sunshine", "AT", "1971-12-04", "-1")
-				);
-		
-		List<ErrorInfo> errors = new DataValidator(withCountry("AT")).check(data);
-		
-		verify(errors.stream().map(ErrorInfo::message).sorted().collect(Collectors.joining("\n")));
-	}
+        List<ErrorInfo> errors = new DataValidator(withCountry("AT")).check(data);
 
-	private CountryInfoService withCountry(String contryCode) {
-		CountryInfoService countryInfoService = new CountryInfoService() {
-			@Override
-			public List<String> getAllCountries() {
-				return List.of(contryCode);
-			}
-		};
-		return countryInfoService;
-	}
+        assertThat(errors).isEmpty();
+    }
+
+
+    @Test
+    void shouldContainNoErrorsIfDataIsEmpty() {
+        Map<Integer, List<String>> data = new HashMap<>();
+        data.put(0, null);
+        data.put(1, List.of());
+
+        List<ErrorInfo> errors = new DataValidator(withCountry("AT")).check(data);
+
+        assertThat(errors).isEmpty();
+    }
+
+    @Test
+    void shouldContainErrorsForEachInvalidEntry() {
+        Map<Integer, List<String>> data = of(
+                1, List.of("Susi", "", "AT", "1971-12-04", "2300.20"),
+                2, List.of("", "Sunshine", "AT", "1971-12-04", "2300.20"),
+                3, List.of("A1", "Sunshine", "AT", "1971-12-04", "2300.20"),
+                4, List.of("Susi", "Sunshine", "DE", "1971-12-04", "2300.20"),
+                5, List.of("Susi", "Sunshine", "AT", "not a date", "2300.20"),
+                6, List.of("Susi", "Sunshine", "AT", "1971-12-04", "not a BigDecimal"),
+                7, List.of("Susi", "Sunshine", "AT", "1971-12-04", "-1")
+        );
+
+        List<ErrorInfo> errors = new DataValidator(withCountry("AT")).check(data);
+
+        verify(errors.stream().map(ErrorInfo::message).sorted().collect(Collectors.joining("\n")));
+    }
+
+    private CountryInfoService withCountry(String contryCode) {
+        return new CountryInfoService() {
+            @Override
+            public List<String> getAllCountries() {
+                return List.of(contryCode);
+            }
+        };
+    }
 
 }
