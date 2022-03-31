0e313cd80ab426bb4b837485406b92e95669aaeb
Katrin Heiderer <katrin.heiderer@nagarro.com>
2022-03-31 12:37:37 +0200

    refactorings and Approvals Tests

--- java/src/test/java/com/craftmanship/DataValidatorTest.java
+++ java/src/test/java/com/craftmanship/DataValidatorTest.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static java.util.Map.of;
 import static org.approvaltests.Approvals.verify;
